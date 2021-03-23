7ec40362a14f724b1991da5f946ad69aa69a01bd
slavabdev <slav.balykov@gmail.com>
2021-03-23 00:21:07 +0000

    Bind data to edit mode

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
     $('select').formSelect();
     $('.datepicker').datepicker({
-        format:'dd, mmmm, yyyy',
+        format:'dd mmmm, yyyy',
         yearRange: 3,
         showClearBtn: true,
         i18n: {
